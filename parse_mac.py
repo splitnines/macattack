56aeb9f98e9278160be1143597fc6a536b835241
splitnines <splitnines@gmail.com>
2025-10-23 16:05:39 -0700

    removed a scratch pad file

--- parse_mac.py
+++
@@ -1,9 +0,0 @@
-def mac_str_to_bytes(mac: str) -> bytes:
-    parts = mac.split(":")
-    if len(parts) != 6:
-        raise ValueError("MAC must be 6 octets separated by ':'")
-    return bytes(int(p, 16) for p in parts)
-
-
-mac = mac_str_to_bytes("52:54:00:06:a7:0f")
-print(mac)
